58f1be0ea3e043779a736b5ec5be93f194545029
Дягилев Денис Сергеевич <ddyagilev@cinimex.ru>
2021-09-07 03:03:15 +0300

    game card

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,32 @@
 import { FunctionComponent } from 'react';
+import './Main.scss';
 
 const Main: FunctionComponent = () => {
-  return <h1>Main</h1>;
+
+  
+  return (
+    <div className="main">
+      <div className="header">
+        <div className="col">
+          <div className="title">Привет 👋</div>
+          <div className="description">Во что бы ты хотел сыграть сегодня?</div>
+        </div>
+      </div>
+      <div className="board">
+        <div className="row">
+          <div className="title">Популярные</div>
+          <div className="games-list"></div>
+        </div>
+
+        <div className="row">
+          <div className="title">
+            <span>Все</span> <span className="action">посмотреть все</span>
+          </div>
+          <div className="games-list"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
